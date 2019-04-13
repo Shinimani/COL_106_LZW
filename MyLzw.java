4fc05926404a0f6cf4d2dd77caf48e4dc290e2ef
Aayush <aayush.somani111@gmail.com>
2019-04-13 23:07:10 +0530

    Wrote complete codes for compress and decompress

--- MyLzw.java
+++ MyLzw.java
@@ -5,44 +5,72 @@ import java.util.List;
 import java.util.Map;
 
 public class MyLzw {
-    private static final int BITS = 12;
-    private static final int MAX_VALUE = (1 << BITS) - 1;
-    private static final int MAX_CODE = MAX_VALUE - 1;
-    private static final int TABLE_SIZE = 5021;
 
-    public static List<Byte> compress(String uncomp)
-    {
+    public static List<Byte> compress(String uncomp) {
 
 
-        Map<String,Integer> mydictionary = new HashMap<String, Integer>();
+        Map<String, Integer> mydictionary = new HashMap<String, Integer>();
         List<Byte> ans = new ArrayList<Byte>();
 
-        for(int i = 0;i<256;i++)
-        {
-            mydictionary.put("" + (char)i,i);
+        for (int i = 0; i < 256; i++) {
+            mydictionary.put("" + (char) i, i);
         }
         String w = "";
         int size = 256;
-        for (char c: uncomp.toCharArray())
-        {
-            String wc = w+c;
-            if(mydictionary.containsKey(wc))
-                w=wc;
-            else
-            {
+        for (char c : uncomp.toCharArray()) {
+            String wc = w + c;
+            if (mydictionary.containsKey(wc))
+                w = wc;
+            else {
 //                String s = "" + mydictionary.get(w);
 //                Byte b = new Byte(s);
                 ans.add(new Byte("" + mydictionary.get(w)));
-                mydictionary.put(wc,size++);
+                mydictionary.put(wc, size++);
                 w = "" + c;
             }
         }
 
-        if (!w.isBlank())
-        {
+        if (!w.isBlank()) {
             ans.add(new Byte("" + mydictionary.get(w)));
         }
         return ans;
     }
+
+    public static String decompress(List<Byte> compressed) {
+        int size = 256;
+        Map<Integer, String> mydictionary = new HashMap<Integer, String>();
+
+        for (int i = 0; i < 256; i++) {
+            mydictionary.put(i, "" + (char) i);
+        }
+//        String ans = "";
+        String w = "" + (char) compressed.remove(0).intValue();
+        StringBuffer ans = new StringBuffer(w);
+        for (Byte k : compressed) {
+            int ki = k.intValue();
+            String newentry = "";
+            if (mydictionary.containsKey(ki)) {
+                newentry = mydictionary.get(ki);
+            } else if (ki == size) {
+                newentry = w + w.charAt(0);
+            } else System.out.println("Wrongly compressed at " + ki);
+
+            ans.append(newentry);
+
+            mydictionary.put(size++, w + newentry.charAt(0));
+
+            w = newentry;
+
+        }
+
+
+//        String anss = ans;
+        return ans.toString();
+
+
+    }
+
+
+
 
 }
