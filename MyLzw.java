dad469575340a7803754958b6b2410398735858a
Aayush <aayush.somani111@gmail.com>
2019-04-13 17:09:50 +0530

    gitignore

---
+++ MyLzw.java
@@ -0,0 +1,12 @@
+import javax.naming.SizeLimitExceededException;
+
+public class MyLzw {
+    private static final int BITS = 12;
+    private static final int MAX_VALUE = (1 << BITS) - 1;
+    private static final int MAX_CODE = MAX_VALUE - 1;
+    private static final int TABLE_SIZE = 5021;
+
+    
+
+
+}
